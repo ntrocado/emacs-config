e78c5010f8bed0ed518110eb5aea86f3c1730549
Nuno Trocado <ntrocado@gmail.com>
2021-07-19 19:47:13 +0100

    Fix org-roam stuff

--- init.el
+++ init.el
@@ -150,19 +150,18 @@
 
 (use-package org-roam
   :ensure t
-  :config
-  (org-roam-setup)
-  (setq org-roam-v2-ack t)
+  :init (setq org-roam-v2-ack t)
   :custom
-  (org-roam-directory "/mnt/c/Users/trocado/OneDrive/Roam")
-  (org-roam-completion-system 'ivy)
+  (org-roam-directory (file-truename "/mnt/c/Users/trocado/OneDrive/Roam"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
          ("C-c n i" . org-roam-node-insert)
          ("C-c n c" . org-roam-capture)
          ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today)))
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-setup))
 
 (use-package org-ref
   :ensure t
