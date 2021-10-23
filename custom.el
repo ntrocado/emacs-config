49aabc4c464523ab4d2c6789cf1bee67d443cc78
Nuno Trocado <ntrocado@gmail.com>
2021-10-23 18:08:57 +0100

    Custom.el update

--- custom.el
+++ custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
+ '(org-agenda-files
+   '("/mnt/c/Users/trocado/OneDrive/Escritorio/notas.org" "/mnt/c/Users/trocado/OneDrive/Documents/diário.org"))
  '(package-selected-packages
-   (quote
-    (emacsql-sqlite nm counsel-notmuch org-roam-bibtex ivy-bibtex org-ref counsel org-plus-contrib helm org-roam magit company-mode company paredit sly notmuch w3m use-package smex bbdb))))
+   '(org tablist ivy-rich emacsql-sqlite org-roam-bibtex ivy-bibtex org-ref counsel helm org-roam magit company-mode company paredit sly w3m use-package smex bbdb))
+ '(safe-local-variable-values '((LilyPond-master-file . "score.ly"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
