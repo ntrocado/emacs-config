68b1432443926f6a344ef986722b9c527287c20d
Nuno Trocado <ntrocado@gmail.com>
2022-03-29 16:35:25 +0100

    Add org-roam-ui

--- init.el
+++ init.el
@@ -370,6 +370,20 @@ current."
   :config
   (org-roam-db-autosync-mode))
 
+(use-package org-roam-ui
+  :straight
+    (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+    :after org-roam
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
 (use-package org-ref
   :straight t
   :init
