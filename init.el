70af40921cda8bed503607f2062ae5d82c89a6bc
Nuno Trocado <ntrocado@gmail.com>
2023-11-02 19:55:49 +0000

    Basic, mostly default (working) config

--- init.el
+++ init.el
@@ -112,66 +112,291 @@ current."
           (2 . (rainbow background 1))
           (3 . (rainbow bold 1))
           (t . (regular 1))))
+
+  ;; Vertico recommended configurations
+  
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t)
+
   :config
   ;; Load the theme of your choice:
   (load-theme 'modus-operandi) ;; OR (load-theme 'modus-vivendi)
   :bind ("<f5>" . modus-themes-toggle))
 
+(use-package vertico
+  :straight (:files (:defaults "extensions/*"))
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+(use-package recentf
+  :init
+  (recentf-mode))
+
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
+(use-package orderless
+  :straight t
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  :straight t
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+              ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
+;; Example configuration for Consult
+(use-package consult
+  :straight t
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command) ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer) ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame) ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)	;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)		;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer) ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store) ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop) ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake) ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)	 ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line) ;; orig. goto-line
+         ("M-g o" . consult-outline) ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find) ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history) ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history) ;; orig. isearch-edit-string
+         ("M-s l" . consult-line) ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)	;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history) ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 3. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  ;;;; 4. projectile.el (projectile-project-root)
+  ;; (autoload 'projectile-project-root "projectile")
+  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 5. No project support
+  ;; (setq consult-project-function nil)
+)
+
+(use-package embark
+  :straight t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc.  You may adjust the Eldoc
+  ;; strategy, if you want to see the documentation from multiple providers.
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; OR (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none))))
+  ;(setq embark-prompter #'embark-completing-read-prompter)
+  )
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :straight t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package citar
+  :straight t
+  :init
+  (setq org-cite-global-bibliography '("~/OneDrive/lab/master.bib"))
+  :config
+  (setq citar-library-paths '("~/OneDrive/lab/pdf/")
+	citar-notes-paths '("~/OneDrive/lab/notes/"))
+  :custom
+  (org-cite-insert-processor 'citar)
+  (org-cite-follow-processor 'citar)
+  (org-cite-activate-processor 'citar)
+  (citar-bibliography org-cite-global-bibliography)
+  ;; optional: org-cite-insert is also bound to C-c C-x C-@
+  :bind
+  (:map org-mode-map :package org ("C-c b" . #'org-cite-insert)))
+
+(use-package citar-embark
+  :straight t
+  :after citar embark
+  :config (citar-embark-mode))
+
 (use-package diminish
   :straight t
   :config
-  (mapcar #'diminish '(eldoc-mode visual-line-mode)))
+  (mapcar #'diminish '(eldoc-mode visual-line-mode citar-embark-mode golden-ratio-mode)))
 
-(use-package abbrev-mode
+(use-package abbrev
+  :ensure nil
   :init
   (setq-default abbrev-mode t)
   :config
   (read-abbrev-file (expand-file-name "abbrev_defs" user-emacs-directory))
   (setq save-abbrevs 'silently))
-
-(use-package ivy
-  :straight t
-  :diminish ivy-mode)
-
-(use-package counsel
-  :straight t
-  :after ivy
-  :diminish counsel-mode
-  :config
-  (defun ivy-call-number (n)
-    (interactive
-     (list (let* ((type (event-basic-type last-command-event))
-		  (char (if (characterp type)
-			    ;; Number on the main row.
-			    type
-			  ;; Keypad number, if bound directly.
-			  (car (last (string-to-list (symbol-name type))))))
-		  (n (- char ?0)))
-	     (if (zerop n) 10 n))))
-    (ivy-set-index (1- n))
-    (ivy--exhibit)
-    (ivy-done))
-  
-  (dotimes (i 10)
-    (define-key ivy-minibuffer-map (read-kbd-macro (format "M-%d" i)) 'ivy-call-number))
-  
-  (setq ivy-use-virtual-buffers t
-	ivy-count-format "(%d/%d) ")
-  (ivy-mode 1)
-  (counsel-mode 1))
-
-(use-package ivy-rich
-  :straight t
-  :after ivy
-  :init
-  (ivy-rich-mode 1)
-  (setq ivy-rich-project-root-cache-mode t)
-  (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line))
-
-(use-package amx
-  :straight t
-  :after ivy
-  :config (amx-mode 1))
 
 (use-package dired
   :ensure nil
@@ -422,48 +647,14 @@ current."
 
 ;;; ORG-REF and IVY-BIBTEX
 
-(defun generic-path (path)
-  (expand-file-name path (cond
-			  ((eql system-type 'windows-nt) "c:/Users/trocado/")
-			  ((eql system-type 'gnu/linux) "~/")))) ;wsl
 
-(defun lab-path (file)
-  (expand-file-name file "~/OneDrive/lab/"))
-
-(use-package ivy-bibtex
-  :straight t
-  :after ivy
-  :bind ("<f12>" . ivy-bibtex)
-  :config
-  (setq bibtex-completion-bibliography (lab-path "master.bib")
-	bibtex-completion-library-path (lab-path "pdf/")
-	bibtex-completion-notes-path (lab-path "notes.org")
-	bibtex-completion-additional-search-fields '(keywords)
-	bibtex-completion-display-formats '((t . "${author:18} ${title:*} ${year:4} ${=has-pdf=:1}${=has-note=:1} ${=type=:7} ${keywords:7}"))
-	bibtex-completion-pdf-symbol "#"
-	bibtex-completion-pdf-extension '(".pdf" ".djvu" ".epub")
-	bibtex-completion-notes-symbol "n"
-
-	bibtex-autokey-name-case-convert-function 'capitalize
-	bibtex-autokey-year-length 4
-	bibtex-autokey-name-year-separator "_"
-	bibtex-autokey-titlewords 0
-
-	bibtex-dialect 'biblatex)
-  
-  (setq bibtex-completion-pdf-open-function
-	(if (eq system-type 'windows-nt)
-	    (lambda (fpath)
-	      (w32-shell-execute "open" fpath))
-	  'org-open-file))
-
-  (defun my/sci-hub ()
-    "Opens a browser for Sci-hub with the bibtex entry at point."
-    (interactive)
-    (browse-url (concat "https://sci-hub.se/"
-			(replace-regexp-in-string
-			 "https?://\\(dx.\\)?.doi.org/" ""
-			 (bibtex-autokey-get-field "doi"))))))
+;;   (defun my/sci-hub ()
+;;     "Opens a browser for Sci-hub with the bibtex entry at point."
+;;     (interactive)
+;;     (browse-url (concat "https://sci-hub.se/"
+;; 			(replace-regexp-in-string
+;; 			 "https?://\\(dx.\\)?.doi.org/" ""
+;; 			 (bibtex-autokey-get-field "doi"))))))
 
 (use-package org-roam
   :straight (:host github :repo "org-roam/org-roam" :branch "main"
@@ -503,28 +694,28 @@ current."
           org-roam-ui-update-on-save t
           org-roam-ui-open-on-start t))
 
-(use-package org-ref
-  :straight t
-  :after org
-  :init
-  (define-key org-mode-map (kbd "C-c c") 'org-ref-insert-link)
-  (setq org-ref-default-citation-link "autocite")
-  (require 'org-ref-ivy)
-  :config
-  ;; Count references: https://github.com/jkitchin/org-ref/issues/1034
-  (defun my/count-refs ()
-    (interactive)
-    (let* ((cites (org-element-map (org-element-parse-buffer) 'link
-		    (lambda (lnk)
-		      (when (member (org-element-property :type lnk)
-				    (mapcar 'car org-ref-cite-types))
-			(cl-loop for ref in (plist-get (org-ref-parse-cite-path
-							(org-element-property :path lnk))
-						       :references)
-				 collect (plist-get ref :key))))))
-	   (all-cites (-flatten cites))
-	   (uniq-cites (-uniq all-cites)))
-      (message "%d references" (length uniq-cites)))))
+;; (use-package org-ref
+;;   :straight t
+;;   :after org
+;;   :init
+;;   (define-key org-mode-map (kbd "C-c c") 'org-ref-insert-link)
+;;   (setq org-ref-default-citation-link "autocite")
+;;   (require 'org-ref-ivy)
+;;   :config
+;;   ;; Count references: https://github.com/jkitchin/org-ref/issues/1034
+;;   (defun my/count-refs ()
+;;     (interactive)
+;;     (let* ((cites (org-element-map (org-element-parse-buffer) 'link
+;; 		    (lambda (lnk)
+;; 		      (when (member (org-element-property :type lnk)
+;; 				    (mapcar 'car org-ref-cite-types))
+;; 			(cl-loop for ref in (plist-get (org-ref-parse-cite-path
+;; 							(org-element-property :path lnk))
+;; 						       :references)
+;; 				 collect (plist-get ref :key))))))
+;; 	   (all-cites (-flatten cites))
+;; 	   (uniq-cites (-uniq all-cites)))
+;;       (message "%d references" (length uniq-cites)))))
 
 
 ;;; SPELLING
