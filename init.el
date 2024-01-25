c72aed660580957909d410133a5f6567485bdd37
Nuno Trocado <ntrocado@gmail.com>
2024-01-25 23:48:49 +0000

    Dired: order by time

--- init.el
+++ init.el
@@ -408,7 +408,7 @@ current."
 
 (use-package dired
   :ensure nil
-  :custom (dired-listing-switches "-lAhv --group-directories-first")
+  :custom (dired-listing-switches "-lAhvtu")
   :config
   (put 'dired-find-alternate-file 'disabled nil)
   (setq ls-lisp-use-insert-directory-program nil
